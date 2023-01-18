014c666d46dbf654fe7d428840931c6e9a0f0442
Light Ediand <lightediand@mail.com>
2023-01-18 19:20:51 +0000

    liv was here

--- happy.py
+++ happy.py
@@ -1,37 +1,13 @@
 def happy(n: int):
-    happy_nums = []
-    for x in range(n):
-        if x==1:
-            happy_nums.append(x)
-        else:
-            init_val = x
-            if x > 9:
-                while x > 9:
-                    beep = []
-                    for i in range(len(str(x))):
-                        y=str(x)
-                        a=int(y[i])
-                        beep.append(a**2)
-                    b=sum(beep)
-                    x=b
-                if x!=1:
-                    pass
-                else:
-                    happy_nums.append(init_val)
-            else:
-                init_val = x
-                b=x**2
-                while b > 9:
-                    boop = []
-                    for i in range(len(str(b))):
-                        y=str(b)
-                        a=int(y[i])
-                        boop.append(a**2)
-                    c=sum(boop)
-                    b=c
-                if b!=1:
-                    pass
-                else:
-                    happy_nums.append(init_val)
-                
+    happy_nums = [1]
+
+    for i in range(2, n):
+        x = i**2 if i < 10 else i
+
+        while x > 9:
+            x = sum([int(digit)**2 for digit in str(x)])
+
+        if x == 1:
+            happy_nums.append(i)
+
     return happy_nums
