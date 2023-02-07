e6531e9731b10331a94a77ebb9fcc9635a245d5c
Light Ediand <lightediand@mail.com>
2023-02-07 03:25:59 +0000

    added primes

--- nombers.py
+++ nombers.py
@@ -6,6 +6,7 @@ from even import even
 from square import square
 from triangle import tri
 from happy import happy
+from primes import primes
 
 print("Hello and welcome to Nombers!\nYou want numbers?\nWe got numbers.\nHere are the types of numbers we currently have to offer.\nPlease place your order!\n")
 
@@ -18,6 +19,7 @@ print("4: Even numbers")
 print("5: Square numbers")
 print("6: Triangle numbers")
 print("7: Happy numbers")
+print("8: Prime numbers")
 
 number_type = input("\nChoose the type of numbers you wish to generate: ")
 
@@ -26,6 +28,6 @@ number = input("Specify an upper bound for your numbers: ")
 t = int(number_type)
 n = int(number)
 
-funcs = [nat, odd, ppt, fib, even, square, tri, happy]
+funcs = [nat, odd, ppt, fib, even, square, tri, happy, primes]
 
 print(funcs[t](n))
